60ef79932b684798e0e044d79312bcc9e2fc8f37
Bruce Porter Jr. <realbruceporter@gmail.com>
2026-06-24 12:11:05 -0400

    Force disable Turbopack properly

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: false,
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  turbopack: false,     // Force Webpack instead of Turbopack
 };
 
 export default nextConfig;
