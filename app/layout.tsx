c02310562992c1d32261749b1f29378084c05b91
Bruce Porter Jr. <realbruceporter@gmail.com>
2026-06-24 02:18:48 -0400

    Fix wallet provider import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { WalletProviderWrapper as WalletProvider } from './providers/WalletProvider';
-import '@solana/wallet-adapter-react-ui/styles.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
